f5ded7650623b8e74b44ca0fd368930330a575a8
0nemor3 <reallucas3@gmail.com>
2021-04-23 16:54:16 +0200

    maj page height

--- questions.php
+++ questions.php
@@ -27,7 +27,7 @@
 			<div class="top">
 				<div class="top__title">
 					<h1>All Questions</h1>
-					<button>Ask Question</button>
+					<button onclick="">Ask Question</button>
 				</div>
 				<div class="top__filter">
 					<p class="top__numbers">
@@ -46,7 +46,47 @@
 					<button class="top__button-karma">Sort by Karma</button>
 				</div>
 			</div>
+			<?php
+				//Connexion à la base de données
+				$link = mysqli_connect(HOST, USER, PASSWORD, BASE);
+				$query = "SELECT titre, question, date, auteur, reponses, karma FROM QUESTIONS;";
+				$resultquestion = mysqli_query($link, $query);
 
+				while ($questionline = mysqli_fetch_array($resultquestion)) {
+					$titre = $questionline['titre'];
+					$question = $questionline['question'];
+					$date = $questionline['date'];
+					$auteur = $questionline['auteur'];
+					$reponses = $questionline['reponses'];
+					$karma = $questionline['karma'];
+			?>
+			<div class="question">
+				<div class="question__top">
+					<div class="question__left">
+						<p class="question__reponses">
+							Réponses<br>
+							<strong><?php echo $reponses; ?></strong>
+						</p>
+						<p class="question__karma">
+							Karma<br>
+							<strong><?php echo $karma; ?></strong>
+						</p>
+					</div>
+					<div class="question__right">
+						<h2></h2>
+						<p></p>
+					</div>
+				</div>
+				<div class="question__bottom">
+					
+				</div>
+			</div>
+
+
+			<?php
+				}
+				mysqli_close($link);
+			?>
 		</div>
 	</div>
 
